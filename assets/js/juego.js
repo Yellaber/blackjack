789bb47334f519491a127696fafbdc0920bdea4b
Yesid Llamas <yellaber@gmail.com>
2024-02-29 14:28:55 -0500

    juego.js: Modificado

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,35 +1,35 @@
 
-let   mazo             = [];
-const tipos            = ['C', 'D', 'H', 'S'];
-const especiales       = ['A', 'J', 'K', 'Q'];
-const fichas           = [10, 20, 50, 100];
-const dineroJugador    = 500;
+let   mazo               = [];
+const tipos              = ['C', 'D', 'H', 'S'];
+const figuras            = ['A', 'J', 'K', 'Q'];
+const fichas             = [10, 20, 50, 100];
+const dineroJugador      = 500;
 
 let ultimaCarta;
-let dineroInicial      = dineroJugador;
+let dineroInicial        = dineroJugador;
 
-let puntajeJugador     = 0,
-    puntajeComputadora = 0,
-    totalFicha10       = 0,
-    totalFicha20       = 0,
-    totalFicha50       = 0,
-    totalFicha100      = 0,
-    totalApuesta       = 0;
+let puntajeJugador       = 0,
+    puntajeComputadora   = 0,
+    totalFicha10         = 0,
+    totalFicha20         = 0,
+    totalFicha50         = 0,
+    totalFicha100        = 0,
+    totalApuesta         = 0;
 
-const badgeDinero      = document.querySelector('#dinero span');
-const fichasApuesta    = document.querySelectorAll('#fichas a');
-const badges           = document.querySelectorAll('#fichas span');
-const opcionesApuesta  = document.querySelectorAll('#opciones-apuesta button');
-const opcionesJuego    = document.querySelectorAll('#opciones-juego button');
-const spans            = document.querySelectorAll('span');
-const jugadorCarta     = document.querySelector('#jugador-cartas');
-const computadoraCarta = document.querySelector('#computadora-cartas');
+const badgeDinero        = document.querySelector('#dinero span');
+const fichasApuesta      = document.querySelectorAll('#fichas a');
+const badges             = document.querySelectorAll('#fichas span');
+const opcionesApuesta    = document.querySelectorAll('#opciones-apuesta button');
+const opcionesJuego      = document.querySelectorAll('#opciones-juego button');
+const spans              = document.querySelectorAll('span');
+const jugadorCarta       = document.querySelector('#jugador-cartas');
+const computadoraCarta   = document.querySelector('#computadora-cartas');
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList        = [...tooltipTriggerList].map(tooltipTriggerEl => 
                                                         new bootstrap.Tooltip(tooltipTriggerEl));
 
-/* Crea y baraja el juego de cartas */
+/* Crea y baraja las cartas */
 const crearMazo = () => {
     for (let i = 2; i <= 10; i++) {
         for (let tipo of tipos) {
@@ -37,13 +37,13 @@ const crearMazo = () => {
         }
     }
 
-    for (let especial of especiales) {
+    for (let figura of figuras) {
         for (let tipo of tipos) {
-            mazo.push(especial + tipo);
+            mazo.push(figura + tipo);
         }
     }
 
-    /* Baraja el juego de cartas */
+    /* Baraja el mazo de cartas */
     let i = mazo.length;
     while (--i > 0) {
         let randIndex = Math.floor(Math.random() * (i + 1));
@@ -61,15 +61,23 @@ const pedirCarta = () => {
 };
 
 /* Devuelve el valor de la carta selleccionada 
-   A => 11
+   A => 1 ó 11
    J, K, Q => 10
    2C, 2D, 2H, 2S => 2         3C, 3D, 3H, 3S => 3         4C, 4D, 4H, 4S => 4
    5C, 5D, 5H, 5S => 5         6C, 6D, 6H, 6S => 6         7C, 7D, 7H, 7S => 7
    8C, 8D, 8H, 8S => 8         9C, 9D, 9H, 9S => 9
 */
-const valorCarta = (carta) => {
-    const valor = carta.substring(0, carta.length - 1);
-    return (!isNaN(valor)) ? valor * 1 : (valor === 'A') ? 11 : 10;
+const valorCarta = (intento, carta, puntaje) => {
+    const valor      = carta.substring(0, carta.length - 1);
+    let   valorCarta = (!isNaN(valor)) ? valor * 1 : (valor === 'A') ? 1 : 10;
+
+    if ((intento == 2 && puntaje == 1) && valorCarta == 10) {
+        valorCarta = 20;
+    } else if ((intento == 2 && puntaje == 10) && valorCarta == 1) {
+        valorCarta = 11;
+    }
+
+    return valorCarta;
 }
 
 /* Prepara el escenario para una nueva apuesta */
@@ -82,7 +90,7 @@ const nuevaApuesta = (saldoDinero) => {
     puntajeComputadora      = 0;
     dineroInicial           = saldoDinero;
     badgeDinero.innerText   = saldoDinero;
-    
+
     if (cartasJugador.length > 0) {
         cartasJugador.forEach((carta) => carta.remove());
     }
@@ -92,16 +100,15 @@ const nuevaApuesta = (saldoDinero) => {
 };
 
 /* Agrupa las cartas seleccionadas por el Jugador y calcula su puntaje respectivo */
-const turnoJugador = () => {
+const turnoJugador = (intento) => {
     const carta           = pedirCarta();
-    puntajeJugador        = puntajeJugador + valorCarta(carta);
+    puntajeJugador        = puntajeJugador + valorCarta(intento, carta, puntajeJugador);
     const imagenCarta     = document.createElement('img');
     imagenCarta.src       = `./assets/images/${carta}.png`;
     imagenCarta.classList.add('shadow');
     jugadorCarta.append(imagenCarta);
 
     if (puntajeJugador >= 21) {
-        //turnoComputadora(0);
         onDetenerPartida();
     }
 };
@@ -109,7 +116,7 @@ const turnoJugador = () => {
 /* Agrupa las cartas seleccionadas por la Computadora y calcula su puntaje respectivo */
 const turnoComputadora = (intento) => {
     ultimaCarta        = pedirCarta();
-    puntajeComputadora = puntajeComputadora + valorCarta(ultimaCarta);
+    puntajeComputadora = puntajeComputadora + valorCarta(intento, ultimaCarta, puntajeComputadora);
     const imagenCarta  = document.createElement('img');
     if (intento === 2) {
         imagenCarta.src = './assets/images/blue-back.png';
@@ -143,7 +150,7 @@ const onRealizarApuesta = () => {
         badgeDinero.innerText = dineroInicial;
         for (let intento = 1; intento <= 2; intento++) {
             turnoComputadora(intento);
-            turnoJugador();
+            turnoJugador(intento);
         }
         deshabilitarElementos(fichasApuesta);
         deshabilitarElementos(opcionesApuesta);
@@ -167,6 +174,9 @@ const onDetenerPartida = () => {
     const cartas  = document.querySelectorAll('#computadora-cartas img');
     cartas[1].src = `./assets/images/${ultimaCarta}.png`;
     cartas[1].classList.add('shadow');
+    while (puntajeComputadora <= 16) {
+        turnoComputadora(0);
+    }
     setTimeout(() => {
         if (puntajeJugador <= 21 && (puntajeJugador > puntajeComputadora || puntajeComputadora > 21)) {
             alert('Ganaste la partida.');
@@ -226,7 +236,7 @@ opcionesApuesta[1].addEventListener('click', () => onRealizarApuesta());
 
 opcionesJuego[0].addEventListener('click', () => onNuevaPartida());
 
-opcionesJuego[1].addEventListener('click', () => turnoJugador());
+opcionesJuego[1].addEventListener('click', () => turnoJugador(0));
 
 opcionesJuego[2].addEventListener('click', () => onDetenerPartida());
 
